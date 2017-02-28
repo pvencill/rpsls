d8c1d9c6660b8455dec39931b26f0d2941bd60eb
Paul Vencill <pvencill@mm221703-pc.mitre.org>
2017-02-28 15:09:26 -0500

    added some images and changed how we restart

---
+++ client/dist/0.3e4807fc7c40b6f7bd25.hot-update.js
@@ -0,0 +1,11 @@
+webpackHotUpdate(0,{
+
+/***/ "welcome.html":
+/***/ (function(module, exports) {
+
+module.exports = "<template>\n  <section class=\"au-animate container\">\n    <h2 class='row'>${heading}</h2>\n    <div class=\"row\">\n      <div class=\"row lizard\"></div>\n      <img src='/styles/img/Paper.png' alt='A piece of paper' />\n    </div>\n  </section>\n</template>\n";
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC4zZTQ4MDdmYzdjNDBiNmY3YmQyNS5ob3QtdXBkYXRlLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vc3JjL3dlbGNvbWUuaHRtbD82YmJiIl0sInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gXCI8dGVtcGxhdGU+XFxuICA8c2VjdGlvbiBjbGFzcz1cXFwiYXUtYW5pbWF0ZSBjb250YWluZXJcXFwiPlxcbiAgICA8aDIgY2xhc3M9J3Jvdyc+JHtoZWFkaW5nfTwvaDI+XFxuICAgIDxkaXYgY2xhc3M9XFxcInJvd1xcXCI+XFxuICAgICAgPGRpdiBjbGFzcz1cXFwicm93IGxpemFyZFxcXCI+PC9kaXY+XFxuICAgICAgPGltZyBzcmM9Jy9zdHlsZXMvaW1nL1BhcGVyLnBuZycgYWx0PSdBIHBpZWNlIG9mIHBhcGVyJyAvPlxcbiAgICA8L2Rpdj5cXG4gIDwvc2VjdGlvbj5cXG48L3RlbXBsYXRlPlxcblwiO1xuXG5cbi8vLy8vLy8vLy8vLy8vLy8vL1xuLy8gV0VCUEFDSyBGT09URVJcbi8vIC4vc3JjL3dlbGNvbWUuaHRtbFxuLy8gbW9kdWxlIGlkID0gd2VsY29tZS5odG1sXG4vLyBtb2R1bGUgY2h1bmtzID0gMCJdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTs7OzsiLCJzb3VyY2VSb290IjoiIn0=
